7059fcc4eb8fdb58e2086d644e867d927e069494
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-08-07 19:02:55 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,25 @@
-import { useState } from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Nav from './Components/Nav/Nav'
-import Services from './Pages/Services/Services'
-import Gallery from './Pages/Gallery/Gallery'
-import Footer from './Pages/Footer/Footer'
-import Banner from './Components/Banner/Banner'
-import About from './Pages/About/About'
-import Team from './Pages/Team/Team'
+import React from 'react'
+import Form from './Components/Form/Form'
+import Todos from './Components/Todos/Todos';
 
+const App = () => {
 
-
-//IMPORT AOS
-
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-import Hero from './Components/Hero/Hero'
-// ..
-AOS.init();
-
-
-function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
-    <div className="App">
-      <h1>X one template</h1>
-      <Router>
-        <Routes>
-          <Route component={Home}/>
-        </Routes>
-        <Home />
-        <Hero />
-        <About />
-        <Banner />
-        <Gallery />
-        <Services />
-        <Team />
-        <Footer />
-      </Router>
+    <div>
+      R-TODO
+      <Form
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <Todos todos={todos} setTodos={setTodos} />
+      <p>developed by Ricky A</p>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/Components/Banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Banner = () => {
-  return (
-    <div className="Banner bg-gray-800">Banner</div>
-  ) 
-}
-
-export default Banner;
